f1a91b1d542f3d11866a5e8595bdb51f4fc2a930
SankaKotik <56644130+SankaKotik@users.noreply.github.com>
2024-08-10 19:29:57 +0300

    Add files via upload

---
+++ openkeysnet_keyparser.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+for page in range (int (input("Номер последнего добавленного прокси на сайте: "))):
+    url = f'https://openkeys.net/key/{page + 1}/'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        accesskey = soup.find('textarea', {'id': 'accessKey'}).text
+        print (accesskey)
